492d75adec4503bb6a6511303ddbcb5687c15135
cipher <cipher@nervos.org>
2019-11-18 20:21:27 +0800

    update short address examples

--- ckb_addr_test.py
+++ ckb_addr_test.py
@@ -5,21 +5,26 @@
 
 
 import segwit_addr as sa
+import hashlib
+import unittest
+
+def ckbhash():
+    return hashlib.blake2b(digest_size=32, person=b'ckb-default-hash')
 
 
 FORMAT_TYPE_SHORT     = 0x01
 FORMAT_TYPE_FULL_DATA = 0x02
 FORMAT_TYPE_FULL_TYPE = 0x04
 
-CODE_INDEX_SECP256K1 = 0x00
+CODE_INDEX_SECP256K1_SINGLE = 0x00
+CODE_INDEX_SECP256K1_MULTI = 0x01
 
-def generateShortAddress(pk, network = "mainnet"):
-    """ generate a standard secp256k1 short ckb address """
+def generateShortAddress(code_index, args, network = "mainnet"):
+    """ generate a short ckb address """
     hrp = {"mainnet": "ckb", "testnet": "ckt"}[network]
     hrpexp =  sa.bech32_hrp_expand(hrp)
     format_type  = FORMAT_TYPE_SHORT
-    code_index = CODE_INDEX_SECP256K1
-    payload = bytes([format_type, code_index]) + bytes.fromhex(pk)
+    payload = bytes([format_type, code_index]) + bytes.fromhex(args)
     data_part = sa.convertbits(payload, 8, 5)
     values = hrpexp + data_part
     polymod = sa.bech32_polymod(values + [0, 0, 0, 0, 0, 0]) ^ 1
@@ -70,18 +75,42 @@ if __name__ == "__main__":
     # test constant parameters
     SECP256K1_CODE_HASH = "9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8"
     PKBLAKE160 = "b39bbc0b3673c7d36450bc14cfcdad2d559c6c64"
+    PKBLAKE_Alice = "094ee28566dff02a012a66505822a2fd67d668fb"
+    PKBLAKE_Bob = "4643c241e59e81b7876527ebff23dfb24cf16482"
+    PKBLAKE_Cipher = "bd07d9f32bce34d27152a6a0391d324f79aab854"
+    MULTI_SISG_PREFIX = b'1' + b'2' + b'3'
 
-    # test short address functions
-    print("== short address test ==")
+    # test short address (code_hash_index = 0x00) functions
+    print("== short address (code_hash_index = 0x00) test ==")
     args = PKBLAKE160
-    print("sample args to encode:\t", args)
-    addr_short = generateShortAddress(args)
-    print("short address generate:\t", addr_short)
+    print("args to encode:\t\t", args)
+    addr_short = generateShortAddress(CODE_INDEX_SECP256K1_SINGLE, args)
+    print("address generate:\t", addr_short)
     decoded = decodeAddress(addr_short)
-    print("decode address:")
-    print(" - format type:\t", decoded[0])
-    print(" - code index:\t", decoded[1])
-    print(" - args:\t", decoded[2])
+    print(">> decode address:")
+    print(" - format type:\t\t", decoded[0])
+    print(" - code_hash_index:\t", decoded[1])
+    print(" - args:\t\t", decoded[2])
+
+    # test short address (code_hash_index = 0x01) functions
+    print("\n== short address (code_hash_index = 0x01) test ==")
+    multi_sign_script = MULTI_SISG_PREFIX \
+        + bytes.fromhex(PKBLAKE_Cipher) \
+        + bytes.fromhex(PKBLAKE_Alice) \
+        + bytes.fromhex(PKBLAKE_Bob)
+    hasher = ckbhash()
+    hasher.update(multi_sign_script)
+    multi_sign_script_hash = hasher.hexdigest()
+    args = multi_sign_script_hash[:40]
+    print("multi sign script:\t", multi_sign_script.hex())
+    print("args to encode:\t\t", args)
+    addr_short = generateShortAddress(CODE_INDEX_SECP256K1_MULTI, args)
+    print("address generate:\t", addr_short)
+    decoded = decodeAddress(addr_short)
+    print(">> decode address:")
+    print(" - format type:\t\t", decoded[0])
+    print(" - code_hash_index:\t", decoded[1])
+    print(" - args:\t\t", decoded[2])
 
     # test full address functions
     print("\n== full address test ==")
@@ -92,11 +121,11 @@ if __name__ == "__main__":
     addr_full = generateFullAddress("Type", code_hash, args)
     print("full address generate:\t", addr_full)
     decoded = decodeAddress(addr_full)
-    print("decode address:")
-    print(" - format type:\t", decoded[0])
-    print(" - code type:\t", decoded[1])
-    print(" - code_hash:\t", decoded[2])
-    print(" - args:\t", decoded[3])
+    print(">> decode address:")
+    print(" - format type:\t\t", decoded[0])
+    print(" - code type:\t\t", decoded[1])
+    print(" - code hash:\t\t", decoded[2])
+    print(" - args:\t\t", decoded[3])
 
 
 
